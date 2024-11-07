052f1fe5f874d6c6011d1707670da0fe28726802
Sami Raiha <sraiha@c2r6p11.hive.fi>
2024-11-07 14:42:58 +0200

    07.11.24-02:11

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: sraiha <sraiha@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 11:13:33 by sraiha            #+#    #+#             */
-/*   Updated: 2024/11/06 12:23:42 by sraiha           ###   ########.fr       */
+/*   Updated: 2024/11/07 14:37:20 by sraiha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ char	*ft_strtrim(char const *s1, const char *set)
 	char	*start;
 	char	*end;
 
-	if (!*s1 || !*set)
+	if (!*s1)
 		return (ft_strdup(""));
 	start = (char *)s1;
 	end = ft_strlen(s1) + start;
@@ -38,4 +38,15 @@ char	*ft_strtrim(char const *s1, const char *set)
 		end--;
 	res = ft_substr(start, 0, end - start);
 	return (res);
+}
+
+#include <stdio.h>
+int main()
+{
+	char test[] = "Hello world!";
+	char *res;
+
+	res = ft_strtrim(test, "");
+
+	printf("%s", res);
 }
