ba257deb0f1f7cf9aef2f46e0061c9c43e32d1ba
Sami Raiha <sraiha@c2r6p11.hive.fi>
2024-11-11 10:29:49 +0200

    changes

---
+++ testMain.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	char *resutlt;
+	char test[] = "tests string";
+	char set[] = ",./";
+	int numResult;
+
+	printf("");
+
+	return (0);
+}
