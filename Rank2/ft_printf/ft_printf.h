fec624c96ac54e317260bde5c18294499467ea67
Sami Räihä <sami.k.raiha@utu.fi>
2024-12-09 17:53:44 +0200

    add prototypes to header file

--- Rank2/ft_printf/ft_printf.h
+++ Rank2/ft_printf/ft_printf.h
@@ -18,8 +18,14 @@
 # include <stddef.h>
 //#include <io.h> 
 int		ft_printf(const char *format, ...);
-void	ft_putchar(int c, size_t counter);
-void	ft_putstr(char *str, size_t counter);
+void	ft_putchar(int c, size_t *counter);
+void	ft_putstr(char *str, size_t *counter);
+void    ft_putptr(void *ptr, size_t *counter);
+void    ft_putnbr(int nbr, size_t *counter);
+void    ft_putunsigned(unsigned int nbr, size_t *counter);
+void    ft_puthex(unsigned int num, size_t *counter, char *base);
+char    *ft_put_number_by_base(unsigned long long num, char *base);
+
 
 # define HEX_UPPER "0123456789ABCDEF"
 # define HEX_LOWER "0123456789abcdef"
